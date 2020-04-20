c8893ff31c0d6dd27b79a696c504273ab5a0330b
mirzayogy <mirza.yogy@gmail.com>
2020-04-20 10:04:54 +0800

    contoh For

--- pbo1-pertemuan04/src/Main.java
+++ pbo1-pertemuan04/src/Main.java
@@ -14,17 +14,31 @@ public class Main {
 //
 //        System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
 
+//        int isiGelas = 0;
+//        int penuh = 225;
+//
+//        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+//        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+//
+//        do {
+//            isiGelas++;
+//            System.out.println("Sedang mengisi gelas..");
+//            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+//        }while(isiGelas != penuh);
+//
+//        System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+
+
         int isiGelas = 0;
         int penuh = 225;
 
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
 
-        do {
-            isiGelas++;
+        for(isiGelas = 0 ; isiGelas <= penuh ; isiGelas++){
             System.out.println("Sedang mengisi gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-        }while(isiGelas != penuh);
+        }
 
         System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
 
